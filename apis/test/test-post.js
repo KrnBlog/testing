316294e8698fc648a261eb59dac8400c13fde636
Developer-karan-projects <karanarora200229@gmail.com>
2023-02-06 02:16:39 +0530

    added 2 get req

--- apis/test/test-post.js
+++ apis/test/test-post.js
@@ -5,10 +5,12 @@ const mongoose = require("mongoose");
 let MODELUSER = mongoose.model("MODELUSER");
 Router.post("/api/testpost", (req, res) => {
   MODELUSER.find().then((allData) => {
+    let email = req.body.email
+    let password = req.body.password
+    let newUser = new MODELUSER({email,password})
+    newUser.save()
     return res.status(200).json({
-      body: req.body,
-      query: req.query,
-      data: allData,
+      body: req.body
     });
   });
 });
