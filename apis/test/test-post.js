1238e0b8b7bf22c3b290a64b472b832c7fbd6033
Developer-karan-projects <karanarora200229@gmail.com>
2023-02-06 01:49:10 +0530

    added mongo get commit

--- apis/test/test-post.js
+++ apis/test/test-post.js
@@ -1,17 +1,15 @@
-const express = require("express")
-const Router = express.Router()
+const express = require("express");
+const Router = express.Router();
+const mongoose = require("mongoose");
 
-function handleTestpost(req,res){
-    res.json({
-        body:req.body,
-        query:req.query
-    })
-}
-
-Router.post("/api/testpost",(req,res)=>{
-    res.status(200).json({
-        body:req.body,
-        query:req.query
-    })
-})
-module.exports = Router
+let MODELUSER = mongoose.model("MODELUSER");
+Router.post("/api/testpost", (req, res) => {
+  MODELUSER.find().then((allData) => {
+    return res.status(200).json({
+      body: req.body,
+      query: req.query,
+      data: allData,
+    });
+  });
+});
+module.exports = Router;
--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express")
-
-
+const mongoose = require("mongoose")
 const application = express()
 
 
 application.use(express.json())
 
 
+require("./models/test-db")
+
+
 application.use("/",require("./apis/test/test-post"))
+
+
+const MongoObj = { useNewUrlParser: true, useUnifiedTopology: true }
+const MongoUrl = "mongodb+srv://karan:karanarora@cluster0.ujsaxn6.mongodb.net/?retryWrites=true&w=majority"
+function MongoFun(err){
+    if(err){console.log("eroor");}
+    else {console.log("conected ...");}
+}
+
+
+mongoose.set('strictQuery', true);
+mongoose.connect(MongoUrl,MongoObj,(err)=>{MongoFun(err)})
 
 
 console.log("server has started");  
